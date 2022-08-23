235d94df6dce66f9009d00fbe171623ff2ac7bd4
aapanasov <aapanasov@gmail.com>
2022-08-23 13:58:31 +0300

    feat: add page IDs

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,12 @@
 const API_URL = 'https://team-171.herokuapp.com/';
 
+export const enum PageIds {
+  MainPage = 'main',
+  GamesPage = 'games',
+  TextbookPage = 'textbook',
+  StatsPage = 'stats',
+  SprintPage = 'sprint',
+  ChallengePage = 'challenge',
+}
+
 export default { API_URL };
