500f22f18e026716d80a0218d895f3f31759a368
aapanasov <aapanasov@gmail.com>
2022-09-01 13:10:17 +0300

    feat: add util funcs

---
+++ src/pages/games/utils/constants.ts
@@ -0,0 +1,16 @@
+export const enum PageIds {
+  MainPage = 'main',
+  GamesPage = 'games',
+  TextbookPage = 'textbook',
+  StatsPage = 'stats',
+  SprintPage = 'sprint',
+  ChallengePage = 'challenge',
+}
+
+export const pagesCount = 30;
+
+export const challengeGameLength = 20;
+
+export const variantsPerQuestion = 4;
+
+export default {};
